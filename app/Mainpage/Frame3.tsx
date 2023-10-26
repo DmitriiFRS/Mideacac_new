d234edf4047b79b4b9e5d00030bf45fd95c1ce43
Дмитрий <Dmitriifrs@gmail.com>
2023-10-26 17:47:18 +0500

    fix

--- app/Mainpage/Frame3.tsx
+++ app/Mainpage/Frame3.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import bg from "../../public/img/frame3bg.jpg";
+import bg3 from "../../public/img/frame3bg.jpg";
 import Frame3Video from "./Frame3Video";
 import Frame3Typography from "./Frame3Typography";
 
@@ -9,7 +9,7 @@ function Frame3() {
       <div className="frame3__container container h-full flex">
         <div className="frame3__img absolute left-0 top-0 w-full h-full">
           <div className="main__imgBody relative w-full h-full">
-            <Image src={bg} alt="" fill={true} priority={true} quality={100} />
+            <Image src={bg3} alt="" fill={true} priority={true} quality={100} />
           </div>
         </div>
         <div className="frame3__content flex justify-between relative z-10 items">
