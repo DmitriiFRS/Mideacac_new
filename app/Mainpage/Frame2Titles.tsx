c719e4338a42e913245b90f8979489ce0236117a
Дмитрий <Dmitriifrs@gmail.com>
2023-10-26 10:50:20 +0500

    anim

--- app/Mainpage/Frame2Titles.tsx
+++ app/Mainpage/Frame2Titles.tsx
@@ -1,13 +1,11 @@
+"use client";
+
 import Image from "next/image";
 import line from "../../public/img/line.png";
-const titles = [
-  "Минимальный шум: благодаря электромагнитной технологии снижается уровень шума. Работа оборудования комфортной и приемлемой для окружающей среды.",
-  "Долговечность: Инновационные технологии и отсутствие трения увеличивают срок службы оборудования и сокращают затраты на обслуживание и ремонт.",
-  "Компактность: Усовершенствованная конструкция чиллеров и теплообменника обеспечивает их удобное и компактное размещение.",
-  "Эффективность: Высокая энергоэффективность снижает потребление энергии, позволяя экономить средства.",
-];
+import { useInView } from "react-intersection-observer";
+import { useEffect, useState } from "react";
 
-const test = [
+const titles = [
   [
     "Минимальный шум: благодаря электромагнитной технологии",
     "снижается уровень шума. Работа оборудования комфортной и",
@@ -30,11 +28,24 @@ const test = [
 ];
 
 function Frame2Titles() {
+  const [isView, setIsView] = useState<boolean>(false);
+  const { ref, inView } = useInView({
+    threshold: 0.2,
+  });
+  useEffect(() => {
+    setIsView(inView);
+  }, [inView]);
   return (
-    <ul className="frame2__list relative z-10">
-      {test.map((el, index) => {
+    <ul ref={ref} className="frame2__list relative z-10">
+      {titles.map((el, index) => {
         return (
-          <li key={index} className="frame2__item flex flex-col">
+          <li
+            style={{ transitionDelay: `${index / 5}s` }}
+            key={index}
+            className={`frame2__item ${
+              isView ? "frame2__item__active" : ""
+            } flex flex-col`}
+          >
             <p className="frame2__title">{el[0]}</p>
             <p className="frame2__title">{el[1]}</p>
             {el.length === 3 ? <p className="frame2__title">{el[2]}</p> : ""}
